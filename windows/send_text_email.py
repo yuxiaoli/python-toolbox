2665e196cbf4105c530bab434465b7e45cba6937
Li <c_yuxiao@quicinc.com>
2016-11-16 17:37:32 -0800

    send_text_email updated

--- windows/send_text_email.py
+++ windows/send_text_email.py
@@ -2,24 +2,35 @@
 import smtplib
 # Import the email modules we'll need
 from email.mime.text import MIMEText
-# Here are the email package modules we'll need
-from email.mime.multipart import MIMEMultipart
 
-def send_text_email(from_addr, to_addr, subject, content):
+def send_text_email(addr, subject, content):
+	
+	gmail_user = "yuxiaoli.bot@gmail.com"
+	gmail_pwd = "automatic"
+	FROM = gmail_user	# cheating recipient does not work
+	TO = addr
+	
 	
 	msg = MIMEText(content)
 	msg['Subject'] = subject
-	msg['From'] = from_addr
-	msg['To'] = to_addr
-
-	# Send the message via our own SMTP server.
-	s = smtplib.SMTP('localhost')
-	s.send_message(msg)
-	s.quit()
+	msg['From'] = FROM
+	
+	COMMASPACE = ', '
+	msg['To'] = COMMASPACE.join(TO)
+	
+	
+	# SMTP_SSL Example
+	server_ssl = smtplib.SMTP_SSL("smtp.gmail.com", 465)
+	server_ssl.ehlo() # optional, called by login()
+	server_ssl.login(gmail_user, gmail_pwd)  
+	# ssl server doesn't support or need tls, so don't call server_ssl.starttls() 
+	server_ssl.sendmail(FROM, TO, msg.as_string())
+	#server_ssl.quit()
+	server_ssl.close()
+	print('successfully sent the mail')
 
 def main():
-	send_text_email("c_yuxiao@quicinc.com", "yuxiaoli.qualcomm@gmail.com", "hi from c-yuxiao", "hello, world")
-	send_text_email("test@test.com", "c_yuxiao@quicinc.com, yuxiaoli.qualcomm@gmail.com", "hi from c-yuxiao", "hello, world")
+	send_text_email(["yuxiaoli.qualcomm@gmail.com", "c_yuxiao@quicinc.com"], "test", "hello, world")
 	
 	
 if __name__ == "__main__":
