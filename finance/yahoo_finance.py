6e97f5a1a7a94ef429d1d410fb90bc6458ec94a8
Sean Li <sean.li@cimoninc.com>
2020-05-04 16:48:15 -0700

    Remove circular import

--- finance/yahoo_finance.py
+++ finance/yahoo_finance.py
@@ -1,7 +1,36 @@
+'''
+http://theautomatic.net/yahoo_fin-documentation/
+
+Requirements
+Yahoo_fin requires the following packages to be installed:
+
+ftplib
+io
+pandas
+requests
+requests_html
+
+pip install requests_html
+pip install pandas
+pip install yahoo_fin
+'''
+# import stock_info module from yahoo_fin
+from yahoo_fin import stock_info as si
+
+'''
+pip install yahoo_earnings_calendar
+'''
 import datetime
 from pytz import timezone
 from yahoo_earnings_calendar import YahooEarningsCalendar
 
+
+def get_stock_price(ticker):
+	return si.get_live_price(ticker)
+
+def get_quote_table(ticker):
+	return si.get_quote_table(ticker)
+	
 def get_next_earnings_date(ticker):
 	yec = YahooEarningsCalendar()
 	
@@ -36,4 +65,6 @@ def get_next_earnings_date(ticker):
 	return next_earnings_datetime
 
 if __name__ == "__main__":
+	print(get_stock_price("aapl"))
 	get_next_earnings_date('aapl')
+
