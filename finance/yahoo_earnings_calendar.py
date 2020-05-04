0091013be318f963c77819654a3cbae4d6ba1c3e
yuxiaoli <yuxiaoli.qualcomm@gmail.com>
2020-05-03 20:31:36 -0700

    Reimplement yahoo_earnings_calendar.py

--- finance/yahoo_earnings_calendar.py
+++ finance/yahoo_earnings_calendar.py
@@ -1,11 +1,39 @@
 import datetime
-from scraper import YahooEarningsCalendar
+from pytz import timezone
+from yahoo_earnings_calendar import YahooEarningsCalendar
 
-date_from = datetime.datetime.strptime(
-    'May 5 2017  10:00AM', '%b %d %Y %I:%M%p')
-date_to = datetime.datetime.strptime(
-    'May 8 2017  1:00PM', '%b %d %Y %I:%M%p')
-yec = YahooEarningsCalendar()
-dt = yec.get_next_earnings_date('amzn')
-print(dt)
-print(datetime.datetime.fromtimestamp(dt))
+def get_next_earnings_date(ticker):
+	yec = YahooEarningsCalendar()
+	
+	'''
+	timestamp = yec.get_next_earnings_date(ticker)
+	print(datetime.datetime.fromtimestamp(timestamp))
+	'''
+
+	earnings = yec.get_earnings_of(ticker)
+
+	next_earnings_datetime = datetime.datetime.now();
+	delta_min = datetime.timedelta.max;
+	for earning in earnings:
+		startdatetime = earning['startdatetime']
+		#print(startdatetime)
+		startdatetime = startdatetime[:-1]
+		isodatetime = datetime.datetime.fromisoformat(startdatetime)
+		#print(isodatetime)
+		
+		est = timezone('US/Eastern').localize(isodatetime);
+		pst = est.astimezone(timezone('US/Pacific'))
+		
+		isodatetime = pst.replace(tzinfo=None)
+		now = datetime.datetime.now()
+		delta = isodatetime - now;
+		
+		if (delta.total_seconds() > 0 and delta < delta_min):
+			delta_min = delta
+			next_earnings_datetime = isodatetime
+		
+	print(next_earnings_datetime)
+	return next_earnings_datetime
+
+if __name__ == "__main__":
+	get_next_earnings_date('aapl')
