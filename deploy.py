6932be4bde8e6d639f0d968f07888ab52f369649
Sean Li <sean.li@cimoninc.com>
2020-07-28 14:44:44 -0700

    Add deploy util

---
+++ deploy.py
@@ -0,0 +1,45 @@
+import sys
+import re
+import os
+import shutil
+
+def package(dirname):
+	path, name = os.path.split(dirname)
+	#print(path)
+	#print(name)
+	
+	# Zip folder
+	shutil.make_archive(name, 'zip', dirname)
+	
+
+def deploy(filename):
+	# Update pip
+	cmd = "pip install --upgrade pip"
+	#os.system(cmd)
+
+	path, name = os.path.split(filename)
+	#print(path)
+	#print(name)
+	
+
+	f = open(filename, 'r')
+	txt = f.read()
+	#print(txt)
+	
+	pattern = "^pip install (?P<lib>\S+)\n"
+	libs = re.findall(pattern, txt, re.MULTILINE)
+	#print(libs)
+	
+	libs = set(libs)
+	#print(libs)
+	for lib in libs:
+		cmd = "pip install " + lib + " --target " + path + " --upgrade --quiet"
+		print(cmd)
+		os.system(cmd);
+		
+	#package(path)
+	return
+	
+if __name__ == "__main__":
+	fname = sys.argv[1]
+	deploy(fname)
---
+++ install.py
@@ -0,0 +1,17 @@
+import os
+from deploy import *
+
+def install(filename='.'):
+	for root, dirs, files in os.walk(filename):
+		for file in files:
+			if (file.endswith('.py')):
+				fname = os.path.join(root, file)
+				#print(fname)
+				
+				# Deploy file
+				deploy(fname)
+		
+
+if __name__ == "__main__":
+	install()
+	
